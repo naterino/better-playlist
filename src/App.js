ce3edab5244668085ab2fca772dcac557e701e68
Nate Olier <nate.j.oliver@gmail.com>
2018-03-30 13:27:08 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColor= '#fff';
+let defaultStyle = {
+  color: defaultTextColor
+}
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div className="aggregate" style={{width: "25%", display: "inline-block"}}>
+        <h2 style={defaultStyle}>Number / Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img/>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}><img/>
+      <h3>Playlist Name</h3>
+      <ul style={{'list-style': 'none'}}>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+      </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    let name = 'Nate';
+    let headerStyle = {color: 'red', 'font-style': 'italic'}
+    return (
+      <div className="App">
+      <h1>Title</h1>
+      <Aggregate/>
+      <Aggregate/>
+      <Filter/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
