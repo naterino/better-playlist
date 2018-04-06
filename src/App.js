a74ebee0a742e59784668003a1034880a2cc3359
Nate Olier <nate.j.oliver@gmail.com>
2018-04-06 14:03:42 -0300

    fixed window location error.

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ class App extends Component {
             <Playlist playlist={playlist}/> 
           )}
         </div> : <button onClick = {()=> {
-          window.location= window.location.includes('localhost') 
+          window.location = window.location.href.includes('localhost') 
           ? 'http://localhost:8888/login'
           : 'https://better-playlist-nate.herokuapp.com/login'}
         }
