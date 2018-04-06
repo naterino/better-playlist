251edb21a7923df17a6ea43160efd310b96ecdc3
Nate Olier <nate.j.oliver@gmail.com>
2018-04-06 14:04:17 -0300

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -144,7 +144,7 @@ class App extends Component {
         </div> : <button onClick = {()=> {
           window.location = window.location.href.includes('localhost') 
           ? 'http://localhost:8888/login'
-          : 'https://better-playlist-nate.herokuapp.com/login'}
+          : 'https://nate-playlist-backend.herokuapp.com/login'}
         }
         style={{padding:'20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
       }
